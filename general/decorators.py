52101d010c870e7dd30bcc257ae1fada7640d2c9
dan-ollerenshaw <45263243+dan-ollerenshaw@users.noreply.github.com>
2019-03-21 17:39:03 +0000

    Update decorators.py

--- general/decorators.py
+++ general/decorators.py
@@ -7,15 +7,17 @@ class reduce_args():
         Standard behaviour is to throw a KeyError. By applying this
         decorator you can avoid it.
         
-        Note: when applying this decorator, you should pass in the config
-        dict as an argument, notthe unpacked **config.
+        Note: when applying this decorator, if all your arguments are in the
+        config dict, you don't need to use any arguments when calling the
+        decorated function. However, if the decorated function has arguments outside
+        the config, you do need to specify them.
     """
     def __init__(self, config):
         self.config = config
     
     def __call__(self, f):
-        def wrapped_f(*args):
+        def wrapped_f(*args, **kwargs):
             func_args = f.__code__.co_varnames
             config = {k:v for k,v in self.config.items() if k in func_args}
-            return f(**config)
+            return f(*args, **kwargs, **config)
         return wrapped_f
